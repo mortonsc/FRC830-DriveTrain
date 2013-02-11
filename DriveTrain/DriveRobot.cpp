88490be0b0a917eb5be6467aa826c4b16115ddf0
ratpack <scottemorton@comcast.net>
2013-02-10 19:21:08 -0500

    added gear shifting

--- DriveTrain/DriveRobot.cpp
+++ DriveTrain/DriveRobot.cpp
@@ -6,7 +6,20 @@ class DriveRobot : public IterativeRobot {
 	//PWM channels:
 	static const int DRIVE_RIGHT = 1;
 	static const int DRIVE_LEFT = 2;
-
+	
+	//buttons used to shift into high and low gear
+	static const int SHIFT_HIGH_BUTTON = 5;
+	static const int SHIFT_LOW_BUTTON = 7;
+	
+	static const int GEAR_SHIFT_RIGHT_CHANNEL = 1;
+	static const int GEAR_SHIFT_LEFT_CHANNEL = 2;
+	
+	static const bool HIGH_GEAR = true;
+	static const bool LOW_GEAR = false;
+			
+	Solenoid * right_gear_shift;
+	Solenoid * left_gear_shift;
+	
 	RobotDrive * drive;
 	Gamepad * gamepad;
 	DriverStationLCD * lcd;
@@ -27,6 +40,9 @@ public:
 				left_drive,
 				right_drive
 				);
+		
+		left_gear_shift = new Solenoid(GEAR_SHIFT_LEFT_CHANNEL);
+		right_gear_shift = new Solenoid(GEAR_SHIFT_RIGHT_CHANNEL);
 		
 		gamepad = new Gamepad(1);
 		lcd = DriverStationLCD::GetInstance();
@@ -58,9 +74,26 @@ public:
 		float forwardSpeed = gamepad->GetLeftY();
 		float sideSpeed = gamepad->GetRightX();
 		drive->ArcadeDrive(CurveAcceleration(forwardSpeed), sideSpeed);
-		lcd->PrintfLine(DriverStationLCD::kUser_Line1,"LEFT Y %f",forwardSpeed);
-		lcd->PrintfLine(DriverStationLCD::kUser_Line2,"RIGHT X %f",sideSpeed);
-	
+		
+		if (gamepad->GetNumberedButton(SHIFT_LOW_BUTTON)){
+			right_gear_shift->Set(LOW_GEAR);
+			left_gear_shift->Set(LOW_GEAR);
+		} else if (gamepad->GetNumberedButton(SHIFT_HIGH_BUTTON)){
+			right_gear_shift->Set(HIGH_GEAR);
+			left_gear_shift->Set(HIGH_GEAR);
+		}
+		
+		lcd->PrintfLine(DriverStationLCD::kUser_Line1,"speed: %f",forwardSpeed);
+		lcd->PrintfLine(DriverStationLCD::kUser_Line2,"rotation: %f",sideSpeed);
+		
+		if (right_gear_shift->Get() != left_gear_shift->Get())
+			lcd->PrintfLine(DriverStationLCD::kUser_Line4, "gears mismatched");
+		else if (right_gear_shift->Get() == HIGH_GEAR)
+			lcd->PrintfLine(DriverStationLCD::kUser_Line4, "in high gear");
+		else if (right_gear_shift->Get() == LOW_GEAR)
+			lcd->PrintfLine(DriverStationLCD::kUser_Line4, "in low gear");
+
+		
 		lcd->UpdateLCD();		
 	}
 	
